a602767eafe9a59b51a384bd12c9ac501c4256fc
Mestre137 <anderson.silva.13756@gmail.com>
2026-09-15 14:51:28 -0300

    Add files via upload
    Adicionar arquivos do site

---
+++ script.js
@@ -0,0 +1,129 @@
+const clienteForm = document.getElementById("clienteForm");
+
+const listaClientes = document.getElementById("listaClientes");
+
+const pesquisa = document.getElementById("pesquisa");
+
+
+// Lista temporária de clientes
+let clientes = [];
+
+
+// CADASTRAR CLIENTE
+
+clienteForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const telefone = document.getElementById("telefone").value;
+    const email = document.getElementById("email").value;
+    const cidade = document.getElementById("cidade").value;
+    const observacoes = document.getElementById("observacoes").value;
+
+
+    const cliente = {
+
+        id: Date.now(),
+
+        nome: nome,
+
+        telefone: telefone,
+
+        email: email,
+
+        cidade: cidade,
+
+        observacoes: observacoes
+
+    };
+
+
+    clientes.push(cliente);
+
+
+    clienteForm.reset();
+
+
+    mostrarClientes();
+
+});
+
+
+// MOSTRAR CLIENTES
+
+function mostrarClientes(lista = clientes) {
+
+    listaClientes.innerHTML = "";
+
+
+    if (lista.length === 0) {
+
+        listaClientes.innerHTML = `
+            <p class="vazio">
+                Nenhum cliente cadastrado.
+            </p>
+        `;
+
+        return;
+    }
+
+
+    lista.forEach(function(cliente) {
+
+        const div = document.createElement("div");
+
+        div.classList.add("cliente");
+
+
+        div.innerHTML = `
+
+            <h3>${cliente.nome}</h3>
+
+            <p>
+                📞 ${cliente.telefone || "Não informado"}
+            </p>
+
+            <p>
+                📧 ${cliente.email || "Não informado"}
+            </p>
+
+            <p>
+                📍 ${cliente.cidade || "Não informado"}
+            </p>
+
+            ${
+                cliente.observacoes
+                ? `<p>📝 ${cliente.observacoes}</p>`
+                : ""
+            }
+
+        `;
+
+
+        listaClientes.appendChild(div);
+
+    });
+
+}
+
+
+// PESQUISAR CLIENTE
+
+pesquisa.addEventListener("input", function() {
+
+    const texto = pesquisa.value.toLowerCase();
+
+
+    const resultado = clientes.filter(function(cliente) {
+
+        return cliente.nome
+            .toLowerCase()
+            .includes(texto);
+
+    });
+
+
+    mostrarClientes(resultado);
+
+});
